ee39b95752415764df6d6b9cfb0fc39702176e27
David <david@linuxsoftware.co.nz>
2022-05-03 17:03:05 +1200

    Sending emails via SMTP2Go

--- lib/outbox.py
+++ lib/outbox.py
@@ -1,12 +1,12 @@
 import asyncio
 from email.message import EmailMessage
 import aiosmtplib
-from secret import EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
+from secret import FROM_EMAIL_ADDRESS, EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 
 async def send_rate_email(to_email, from_code, to_code, rate):
     # FIXME check this is a valid email address?
     message = EmailMessage()
-    message["From"]    = "noreply@linuxsoftware.nz"
+    message["From"]    = FROM_EMAIL_ADDRESS
     message["To"]      = to_email
     message["Subject"] = "Exchange Rate"
     message.set_content(f"""
@@ -17,5 +17,4 @@ The exchange rate from {from_code} to {to_code} is {rate}.
                           port=465,
                           username=EMAIL_HOST_USER,
                           password=EMAIL_HOST_PASSWORD,
-                          use_tls=True,
-                          validate_certs=False)
+                          use_tls=True)
